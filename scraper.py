c8c8a99908dcf144460cbde4c309c876ab86cb40
Christopher Roach <c.roach10@yahoo.com>
2018-10-31 20:38:21 -0400

    successfully obtain all twitter handles from a list of sites in a text file

--- scraper.py
+++ scraper.py
@@ -1,16 +1,25 @@
 import requests
 import re
 
-url = 'http://programmingchris.com/'
-page_data = requests.get(url)
-
 regex = 'https://twitter.com/[a-zA-Z0-9$&+,:;=?@#|<>.^*''()%!-]*'
 
+file = input("enter file path: ")
 
-twitter_handle = re.findall(regex, page_data.text)
+
+sites = [site.rstrip('\n') for site in open(file)]
+for site in sites:
+    page_data = requests.get(site)
+    twitter_handle = re.findall(regex, page_data.text)
+    for handle in twitter_handle:
+        handle = handle[8:]
+        returned_handle = '@' + handle[12:]
+        print(returned_handle)
 
 
-for handle in twitter_handle:
-    handle = handle[8:]
-    returned_handle = '@' + handle[12:]
-    print(returned_handle)
+
+
+
+
+
+
+
